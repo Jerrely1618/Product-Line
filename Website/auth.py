78bee142a1450880029b5152da0c810b19b78d23
Jerrely1618 <victorv2718@gmail.com>
2022-11-22 22:34:05 -0500

    Created HTML form for SIgn Up

--- Website/auth.py
+++ Website/auth.py
@@ -1,13 +1,48 @@
 #Login/Sign_up pages
 
-from flask import Blueprint, render_template
+from flask import Blueprint,render_template,request
+
+class GuestApplication:
+    name = ""
+    credit_card = ""
+    address = ""
+    email = ""
+    password = ""
+    phone_num = ""
+    
+    def __init__(self,name,credit_card,address,phone,email,password) -> None:
+        self.name = name
+        self.credit_card = credit_card
+        self.address = address
+        self.email = email
+        self.phone_num = phone
+        self.password = password
 
 verify_page = Blueprint('verify_page',__name__)
 
-@verify_page.route('/login')
+@verify_page.route('/login',methods=['POST','GET'])
 def login():
     return render_template("login.html")
 
-@verify_page.route('/sign-up')
+@verify_page.route('/sign-up',methods=['POST','GET'])
 def sign_up():
+    if request.method=="POST":
+        username = request.form['name']
+        email = request.form['email']
+        VerifiedEmail = request.form['email_ver']
+        Password = request.form['password']
+        VerifiedPassword = request.form['password_ver']
+        phoneNum = request.form['phone']
+        address = request.form['address']
+        creditCard = request.form['Credit_card']
+        if email != VerifiedEmail:
+            print("Wrong email!")
+        if Password != VerifiedPassword:
+            print("Password wrong!")
+        
+        newGuestApp = GuestApplication(username,creditCard,address,phoneNum,VerifiedEmail,VerifiedPassword)
+        # Here we put the guestApp on a database  
+        print(newGuestApp.name)
+    else:
+        username = request.args.get("name")
     return render_template("sign-up.html")
